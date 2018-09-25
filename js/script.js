5e22badb69aa7a39cfe46b6592c3fd78381ed012
Agata <agatamagata91@gmail.com>
2018-09-25 19:00:12 +0300

    Модельные окна встроенные в браузер

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// let name = document.getElementById('name'),
+//     surname = document.getElementById('surname'),
+//     age = document.getElementById('age'),
+//     nameInput = document.getElementsByTagName('input')[0],
+//     surnameInput = document.getElementsByTagName('input')[1],
+//     ageInput = document.getElementsByTagName('input')[2];
+
+// nameInput.addEventListener('input', function(){
+//     name.innerHTML = nameInput.value
+// });
+// surnameInput.addEventListener('input', function(){
+//     surname.innerHTML = surnameInput.value
+// });
+// ageInput.addEventListener('input', function(){
+//     age.innerHTML = ageInput.value
+// });
+let name = document.querySelector('#name'),
+    surname = document.querySelector('#surname'),
+    age = document.querySelector('#age'),
+    input = document.querySelectorAll('input');
+
+function show(a,b) {
+    input[a].addEventListener('input', function(){
+        b.innerHTML = input[a].value
+    });
+}
+show(0, name);
+show(1, surname);
+show(2, age);
