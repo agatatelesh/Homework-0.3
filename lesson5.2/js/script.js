79985eb4a513daef4e6a6b471256139dbb4988f2
Agata <agatamagata91@gmail.com>
2018-10-11 16:05:04 +0300

    Доступ элементов на странице. Часть 2

---
+++ lesson5.2/js/script.js
@@ -0,0 +1,17 @@
+let start = document.querySelector('#start'),
+    budgetValue = document.querySelector('.budget-value'),
+    daybudgetValue= document.querySelector('.daybudget-value'),
+    levelValue = document.querySelector('.level-value'),
+    expensesValue = document.querySelector('.expenses-value'),
+    optionalexpensesValue = document.querySelector('.optionalexpenses-value'),
+    incomeValue = document.querySelector('.income-value'),
+    monthsavingsValue = document.querySelector('.monthsavings-value'),
+    yearsavingsValue = document.querySelector('.yearsavings-value'),
+    expensesItem = document.querySelectorAll('.expenses-item'),
+    btnExpenses = document.querySelectorAll('button')[0],
+    btnOptionalexpenses = document.querySelectorAll('button')[1],
+    btnCountBudget = document.querySelectorAll('button')[2],
+    optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    a = document.querySelectorAll("input");
+
+console.log(a);
